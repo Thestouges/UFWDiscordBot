8b9cae2d54d371f208a05037c992c11bbd6700b3
Thestouges <thestouges@gmail.com>
2017-03-07 12:52:09 -0500

    Added command to join and leave voice channel

--- UFWDiscordBot/UFWBot.cs
+++ UFWDiscordBot/UFWBot.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using Discord.Audio;
 
 namespace UFWDiscordBot
 {
@@ -12,26 +13,40 @@ namespace UFWDiscordBot
     {
         DiscordClient _client;
         CommandService commands;
+        Channel voiceChannel;
+        bool inVoiceChannel;
 
         public UFWBot()
         {
-            _client = new DiscordClient(x =>
-            {
-                x.LogLevel = LogSeverity.Info;
-                x.LogHandler = Log;
-            });
-
-            _client.UsingCommands(x =>
-            {
-                x.PrefixChar = '!';
-                x.AllowMentionPrefix = true;
-            });
+            Initializer();
 
             commands = _client.GetService<CommandService>();
             CommandsList();
 
             _client.ExecuteAndWait(async () => {
                 await _client.Connect("Mjg2OTU1MzQ0MzI2ODE5ODUx.C5odcw.CxZFfrN4xHQ4TrbJfkvN1DpLM0s", TokenType.Bot);
+            });
+        }
+
+        private void Initializer()
+        {
+            inVoiceChannel = false;
+
+            _client = new DiscordClient(x =>
+            {
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });
+
+            _client.UsingAudio(x =>
+            {
+                x.Mode = AudioMode.Outgoing;
+            });
+
+            _client.UsingCommands(x =>
+            {
+                x.PrefixChar = '!';
+                x.AllowMentionPrefix = true;
             });
         }
 
@@ -43,6 +58,8 @@ namespace UFWDiscordBot
         private void CommandsList()
         {
             say();
+            join();
+            leave();
         }
 
         private void say()
@@ -53,6 +70,36 @@ namespace UFWDiscordBot
                 .Do(async e =>
                 {
                     await e.Channel.SendMessage($"{e.User.Name} says {e.GetArg("Message")}");
+                });
+        }
+
+        private void join()
+        {
+            commands.CreateCommand("join")
+                .Description("Joins the users voice channel")
+                .Do(async e =>
+                {
+                    if (e.User.VoiceChannel != null)
+                    {
+                        voiceChannel = e.User.VoiceChannel;
+                        await _client.GetService<AudioService>()
+                            .Join(voiceChannel);
+                        inVoiceChannel = true;
+                    }
+                    else
+                        await e.Channel.SendMessage($"You must be in a voice channel.");
+                });
+        }
+
+        private void leave()
+        {
+            commands.CreateCommand("leave")
+                .Description("Leaves the voice channel")
+                .Do(async e =>
+                {
+                    await _client.GetService<AudioService>()
+                        .Leave(voiceChannel);
+                    inVoiceChannel = false;
                 });
         }
     }
